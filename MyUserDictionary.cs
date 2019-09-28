c96a75f091c4b0ffd008c01e0b6a85c9bc5af6ff
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-09-28 18:53:20 -0400

    update
    cleaned up the code and took out extra/ repeating lines that I did not need

--- MyUserDictionary.cs
+++ MyUserDictionary.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace PasswordEncryptionAuthentication
 {
@@ -35,43 +32,17 @@ namespace PasswordEncryptionAuthentication
 
         public bool Authorize(Credentials credentials)
         {
-            return UserLogin.Any(l => l.Key.ToLower() == credentials.Username.ToLower() && l.Value == credentials.Password);
-        }
-
-
-
-        public static string Encrypt(Credentials credentials)
-        {
-
-            return credentials.Password;
-        }
-        public static string Decrypt(Credentials credentials1)
-        {
-            string EncryptionKey = "abc123";
-            credentials1.Password = credentials1.Password.Replace(" ", "+");
-            byte[] cipherBytes = Convert.FromBase64String(credentials1.Password);
-            using (Aes encryptor = Aes.Create())
+            try
             {
-                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
-                encryptor.Key = pdb.GetBytes(32);
-                encryptor.IV = pdb.GetBytes(16);
-                using (MemoryStream ms = new MemoryStream())
-                {
-                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
-                    {
-                        cs.Write(cipherBytes, 0, cipherBytes.Length);
-                        cs.Close();
-                    }
-                    credentials1.Password = Encoding.Unicode.GetString(ms.ToArray());
-                }
+                return UserLogin.Any(l => l.Key.ToLower() == credentials.Username && l.Value == credentials.Password);
+
             }
-            return credentials1.Password;
+            catch (ArgumentException)
+            {
+                return false;
+            }
         }
-
-
-
     }
-
 }
 
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace PasswordEncryptionAuthentication
 {
@@ -9,65 +10,81 @@ namespace PasswordEncryptionAuthentication
 
             MainScreen mainScreen = new MainScreen();
             MyUserDictionary myUserDictionary = new MyUserDictionary();
+            //  AesE aes = new AesE();
+
+
             mainScreen.Display();
 
             int menuoptions = Convert.ToInt32(Console.ReadLine());
-            try
+            //try
+            //{
+
+            while (menuoptions <= 3)
             {
+                Credentials credentials = new Credentials();
 
-                while (menuoptions <= 3)
+                switch (menuoptions)
                 {
-                    Credentials credentials = new Credentials();
 
-                    switch (menuoptions)
-                    {
-
-                        case 1:
+                    case 1:
 
 
-                            Console.WriteLine("Enter your username: ");
+                        Console.WriteLine("Enter your username: ");
+                        credentials.Username = Console.ReadLine();
+                        Console.WriteLine("Enter your password: ");
+                        credentials.Password = Console.ReadLine();
+
+
+                        while (!myUserDictionary.AddAccount(credentials))
+                        {
+                            //This means there was an error
+                            Console.WriteLine("Enter a username that is not already in use: ");
                             credentials.Username = Console.ReadLine();
-                            Console.WriteLine("Enter your password: ");
+                            Console.WriteLine("Enter a password: ");
                             credentials.Password = Console.ReadLine();
 
+                        };
 
-                            while (!myUserDictionary.AddAccount(credentials))
-                            {
-                                //This means there was an error
-                                Console.WriteLine("Enter a username that is not already in use: ");
-                                credentials.Username = Console.ReadLine();
-                                Console.WriteLine("Enter a password: ");
-                                credentials.Password = Console.ReadLine();
-
-                            };
-
-                            break;
-                        case 2:
-
-                            Console.WriteLine("Your encrypted password is: ");
-                            Console.WriteLine(credentials.Password);
-                            Console.WriteLine(credentials.Username);
-
-                            break;
-                        default:
-                            //here thing exit method
-                            //  Dispose();
-                            break;
+                        break;
+                    case 2:
 
 
+                        Console.WriteLine("Enter your username: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter your password: ");
+                        string pass = Console.ReadLine();
+                        // myUserDictionary.Authorize(credentials);
+                        //while (name != credentials.Username && pass != credentials.Password)
 
-                    }
-                    Console.Clear();
-                    mainScreen.Display();
-                    menuoptions = Convert.ToInt32(Console.ReadLine());
+                        while (!myUserDictionary.Authorize(credentials))
+                        {
+                            Console.WriteLine("Sorry the username or password you entered is not correct. ");
+
+
+                        }
+                        Console.WriteLine($"Welcome back {credentials.Username}");
+
+                        //Console.WriteLine("Your encrypted password is: ");
+                        //Console.WriteLine(credentials.Password);
+                        //Console.WriteLine(credentials.Username);
+                        break;
+                    default:
+                        //here thing exit method
+                        //  Dispose();
+                        break;
+
+
+
                 }
-            }
-            catch
-            {
-
+                Console.Clear();
+                mainScreen.Display();
+                menuoptions = Convert.ToInt32(Console.ReadLine());
             }
         }
+        //catch
+        //{
 
-
+        //}
     }
+
 }
